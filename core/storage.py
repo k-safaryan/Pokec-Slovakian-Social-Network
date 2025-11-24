22cb55de9230bc1cf355a47fabd5a3111b61d976
Seroj30 <seryozha_karapetyan@edu.aua.am>
2025-11-25 00:16:48 +0400

    Update storage.py

--- core/storage.py
+++ core/storage.py
@@ -1,18 +1,14 @@
 import csv
-import collections
 from indexing import AVLTree
 
 class Storage:
-    def __init__(self, data_path, relationships_path):
+    def __init__(self, data_path):
         self.data_path = data_path
-        self.relationships_path = relationships_path
         self.hash_map = {}
         self.age_index = AVLTree()
-        self.graph = collections.defaultdict(list)
 
     def initialize(self):
         self._load_user_data()
-        self._load_relationships()
 
     def _load_user_data(self):
         with open(self.data_path, 'r', encoding='utf-8') as f:
@@ -40,16 +36,6 @@ class Storage:
                 except (ValueError, TypeError) as e:
                     pass
 
-    def _load_relationships(self):
-        with open(self.relationships_path, 'r', encoding='utf-8') as f:
-            for line in f:
-                try:
-                    u1, u2 = map(int, line.strip().split())
-                    self.graph[u1].append(u2)
-                    self.graph[u2].append(u1)
-                except ValueError:
-                    pass
-
     def get_user_by_id(self, user_id):
         return self.hash_map.get(user_id)
 
@@ -66,7 +52,6 @@ class Storage:
             raise ValueError(f"User ID {user_id} already exists.")
         
         self.hash_map[user_id] = user_data
-        self.graph[user_id] = []
         
         if age is not None:
             self.age_index.add(age, user_data)
@@ -80,12 +65,6 @@ class Storage:
 
         if age is not None:
             self.age_index.delete(age, user_data)
-        
-        if user_id in self.graph:
-            for friend_id in self.graph[user_id]:
-                if friend_id in self.graph and user_id in self.graph[friend_id]:
-                    self.graph[friend_id].remove(user_id)
-            del self.graph[user_id]
         
         return True
 
@@ -109,31 +88,3 @@ class Storage:
                 current_data[key] = value
         
         return True
-
-    def find_shortest_path(self, start_id, end_id):
-        if start_id == end_id:
-            return [start_id]
-        if start_id not in self.graph or end_id not in self.graph:
-            return None
-
-        queue = collections.deque([start_id])
-        visited = {start_id}
-        parent = {start_id: None}
-
-        while queue:
-            current_id = queue.popleft()
-
-            if current_id == end_id:
-                path = []
-                while current_id is not None:
-                    path.append(current_id)
-                    current_id = parent[current_id]
-                return path[::-1]
-
-            for neighbor_id in self.graph[current_id]:
-                if neighbor_id not in visited:
-                    visited.add(neighbor_id)
-                    parent[neighbor_id] = current_id
-                    queue.append(neighbor_id)
-        
-        return None
